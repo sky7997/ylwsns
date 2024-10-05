33b14474c791e8893eebeb864192db3cde2c2feb
sky7997 <skyasarthetwothrones@gmail.com>
2024-10-05 22:14:48 +0530

    Update index.js

--- src/components/JobCardPage/index.js
+++ src/components/JobCardPage/index.js
@@ -3,16 +3,60 @@ import { FaBookmark, FaRegBookmark } from "react-icons/fa";
 import "./index.css";
 
 class JobCard extends Component {
+    constructor(props) {
+        super(props);
+        this.touchStartX = 0;
+        this.touchEndX = 0;
+    }
+
     handleBookmarkJob = () => {
-        const { job, onBookmark } = this.props; // Pass the onBookmark function from props
-        onBookmark(job); // Call the onBookmark function
-    };
+        const job = this.props.job;
+        let bookmarkedJobs = JSON.parse(localStorage.getItem('bookmarkedJobs')) || [];
+        const isBookmarked = bookmarkedJobs.some(item => item.id === job.id);
+
+        if (isBookmarked) {
+            bookmarkedJobs = bookmarkedJobs.filter(item => item.id !== job.id);
+        } else {
+            bookmarkedJobs.push(job);
+        }
+
+        localStorage.setItem('bookmarkedJobs', JSON.stringify(bookmarkedJobs));
+        this.forceUpdate(); // Re-render after bookmarking
+    }
+
+    handleTouchStart = (e) => {
+        this.touchStartX = e.changedTouches[0].screenX;
+    }
+
+    handleTouchEnd = (e) => {
+        this.touchEndX = e.changedTouches[0].screenX;
+        this.handleSwipe();
+    }
+
+    handleSwipe = () => {
+        if (this.touchStartX - this.touchEndX > 50) {
+            // Swipe left - Dismiss job
+            this.props.onSwipeLeft();
+        }
+
+        if (this.touchEndX - this.touchStartX > 50) {
+            // Swipe right - Bookmark job
+            this.props.onSwipeRight();
+        }
+    }
 
     render() {
-        const { job, isBookmarked } = this.props; // Get isBookmarked from props
+        const { job } = this.props;
+
+        let bookmarkedJobs = JSON.parse(localStorage.getItem('bookmarkedJobs')) || [];
+        const isBookmarked = bookmarkedJobs.some(item => item.id === job.id);
 
         return (
-            <div className="job-card-container">
+            <div
+                className="job-card-container"
+                onTouchStart={this.handleTouchStart}
+                onTouchEnd={this.handleTouchEnd}
+            >
                 <h1 className="company-name">{job.company_name}</h1>
                 <h1 className="job-title">{job.title}</h1>
                 <p><strong>Location: </strong>{job.primary_details?.Place || 'Not mentioned'}</p>
@@ -23,7 +67,6 @@ class JobCard extends Component {
                     <button onClick={this.handleBookmarkJob} className="bookmark-button">
                         {isBookmarked ? <FaBookmark /> : <FaRegBookmark />}
                     </button>
-                    <a href={`/jobs/${job.id}`} className="nav-item">View Details</a>
                 </div>
             </div>
         );
